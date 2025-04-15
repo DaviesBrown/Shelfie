f266430de8b09f2611757cf946d0d8f9296fd46e
Nwosu David <nwosudavid77@gmail.com>
2025-04-15 14:30:26 +0100

    Colorscheme theming

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,20 +1,25 @@
 import { StyleSheet, Text, useColorScheme, View } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
+import { Colors } from "../constants/Colors"
+import { StatusBar } from 'expo-status-bar'
 
 const App = () => {
   const colorScheme = useColorScheme()
-  console.log(colorScheme)
-  
+  const theme = Colors[colorScheme] ?? Colors.light
+
   return (
-    <Stack screenOptions={{
-        headerStyle: {backgroundColor: '#ddd'},
-        headerTintColor: '#333'
-    }}>
-        <Stack.Screen name='index' options={{title: 'Home'}} />
-        <Stack.Screen name='about' options={{title: 'About'}} />
-        <Stack.Screen name='contact' options={{title: 'Contact', headerShown: false}} />
-    </Stack>
+    <>
+        <StatusBar value="auto" />
+        <Stack screenOptions={{
+            headerStyle: {backgroundColor: theme.navBackground},
+            headerTintColor: theme.title
+        }}>
+            <Stack.Screen name='index' options={{title: 'Home'}} />
+            <Stack.Screen name='about' options={{title: 'About'}} />
+            <Stack.Screen name='contact' options={{title: 'Contact', headerShown: false}} />
+        </Stack>
+    </>
   )
 }
 
