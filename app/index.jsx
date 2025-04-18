86593ea320d876e52086c6560dc39a1aafd1b252
Nwosu David <nwosudavid77@gmail.com>
2025-04-18 16:31:35 +0100

    spacer component

--- app/index.jsx
+++ app/index.jsx
@@ -1,18 +1,19 @@
 import { Image, StyleSheet, Text, View } from 'react-native'
-import Logo from '../assets/img/shelfie.png'
+//import Logo from '../assets/img/shelfie.png'
 import React from 'react'
 import { Link } from 'expo-router'
 import ThemedView from '../components/ThemedView'
+import ThemedLogo from '../components/ThemedLogo'
+import Spacer from '../components/Spacer'
 
 const Home = () => {
     return (
         <ThemedView style={styles.container}>
-            <Image source={Logo} style={styles.img}></Image>
-            <Text style={styles.title}>Home</Text>
-            <Text style={{margin: 10}}>Reading List App</Text>
-            <View style={styles.card}>
-                <Text>Hello, this is a card.</Text>
-            </View>
+            <ThemedLogo style={styles.img} />
+            <Spacer height={10} />
+            <Text>Reading List App</Text>
+            <Spacer height={100} />
+
             <Link href="/about" style={styles.link}>About Page</Link>
             <Link href="/contact" style={styles.link}>Contact Page</Link>
         </ThemedView>
--- components/ThemedLogo.jsx
+++ components/ThemedLogo.jsx
@@ -2,12 +2,12 @@ import { Image, useColorScheme } from 'react-native'
 import DarkLogo from "../assets/img/shelfie-dark.png"
 import LightLogo from "../assets/img/shelfie-light.png"
 
-const ThemedLogo = () => {
+const ThemedLogo = ({...props}) => {
   const colorScheme = useColorScheme()
   const logo = colorScheme === "dark" ? DarkLogo : LightLogo
 
   return (
-    <Image source={logo} />
+    <Image source={logo} {...props} />
   )
 }
 
